de3f3e0cda53ceedfa8b210a35790ad012ad8080
LaszloChung <j0880040@gmail.com>
2018-03-15 04:20:34 +0800

    Ready for new weather format at 4/2

--- motd.sh
+++ motd.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 #### Color ####
-NON="\033[0m"  # unsets color to term's fg color
-K="\033[0;30m"  # black
-R="\033[0;31m"  # red
-G="\033[0;32m"  # green
-Y="\033[0;33m"  # yellow
-B="\033[0;34m"  # blue
-M="\033[0;35m"  # magenta
-C="\033[0;36m"  # cyan
-W="\033[0;37m"  # white
-
-# emphasized (bolded) colors
+NON="\033[m"  # unsets color to term's fg color
 EMK="\033[1;30m"
 EMR="\033[1;31m"
 EMG="\033[1;32m"
@@ -22,7 +12,7 @@ EMC="\033[1;36m"
 EMW="\033[1;37m"
 
 #### MOTD ####
-motd="${EMY}Welcome! Laszlo${Y}${NON}" #SAY SOMETHING
+motd=${EMY}"Welcome! Laszlo"${NON} #SAY SOMETHING
 
 echo -e "  _________________\n" \
 	"< $motd >\n" \
@@ -40,33 +30,34 @@ echo -e "  _________________\n" \
 
 #### DISK USAGE ####
 echo -n "已用空間 " 
-echo -ne "$EMG$(df -g | grep disk1 | awk '{print $3}')$G$NON / "
-echo -e "$EMR$(df -g | grep disk1 | awk '{print $2}')$R$NON GB\n"
+echo -ne "${EMG}$(df -g | grep disk1 | awk '{print $3}')${NON} / "
+echo -e "${EMR}$(df -g | grep disk1 | awk '{print $2}')${NON} GB\n"
 
-#WEATHER
+#### WEATHER ####
 KEY="" #Your API key at http://opendata.cwb.gov.tw/usages
 wfile="/tmp/weather"
-DATAID1="F-D0047-009" 	# 新竹縣未來2天天氣預報
-			# location:1 五峰鄉 2 峨眉鄉 3 尖石鄉 4 寶山鄉 5 新豐鄉 6 北埔鄉 7 橫山鄉 8 關西鎮 9 竹東鎮 10 湖口鄉 11 芎林鄉 12 竹北市 13 新埔鎮
-DATAID2="F-D0047-053" 	# 新竹市未來2天天氣預報 
-			# location:1 北區 2 香山區 3 東區
-DATAID3="F-D0047-053" 	# 台北市未來2天天氣預報 
-			# location:
-localID=3		# Choose above DATAID's locationID
 intervals=5400		# Second Time to to update file content
-time=2			# Timeout in N seconds
+timeout=2		# Timeout in N seconds
+DATAID="F-D0047-009"
+district="竹東鎮"
+# 新竹縣未來2天天氣預報 F-D0047-009
+# district:峨眉鄉 關西鎮 芎林鄉 湖口鄉 新豐鄉 新埔鎮 橫山鄉 北埔鄉 寶山鄉 五峰鄉 尖石鄉 竹東鎮 竹北市
+# 新竹市未來2天天氣預報 F-D0047-053
+# district:北區 香山區 東區
+# 台北市未來2天天氣預報 F-D0047-061
+# district:南港區 大安區 內湖區 大同區 文山區 士林區 松山區 萬華區 中正區 信義區 中山區 北投區
+# Data List http://opendata.cwb.gov.tw/datalist
 
 [ ! -e ${wfile} ] && touch ${wfile} # Create file
 [ ! -s ${wfile} -o $(($(date +%s)-$(stat -f "%m" ${wfile}))) -gt $intervals ] && \
-curl -m ${time} -s "http://opendata.cwb.gov.tw/opendataapi?dataid=${DATAID2}&authorizationkey=${KEY}" | tail -n +27 | sed '$ d' > ${wfile} 
+curl -m ${timeout} -s "https://opendata.cwb.gov.tw/opendataapi?dataid=${DATAID}&authorizationkey=${KEY}" | sed 's/xmlns=".*"//g' > ${wfile}
 # If file is empty or not latest. Each dataset is at 3 hour intervals and then get Latest Dataset
 
-if [ ! -z $(tail -n 1 ${wfile} | grep "</dataset>") ];then # Check file's completeness or interrupted
+if [ ! -z $(tail -n 1 ${wfile} | grep "cwbopendata") ];then # Check file's completeness or interrupted
 	locations=$(xmllint --xpath 'string(//locationsName)' ${wfile})
-	location=$(xmllint --xpath 'string(//location['${localID}']/locationName)' ${wfile})
-	#endtime=$(xmllint --xpath '//location['${localID}']/weatherElement[10]/time[1]/endTime/text()' ${wfile})
-	weather=$(xmllint --xpath 'string(//location['${localID}']/weatherElement[10]/time[1]/elementValue/value)' ${wfile})
-	echo -e "["${locations}${location}"]  " && echo -e ${weather}"\n" | sed 's/ //g'
+	location=$(xmllint --xpath 'string(//location/locationName[text()="'${district}'"])' ${wfile})
+	weather=$(xmllint --xpath 'string(//location/locationName[text()="'${district}'"]/following-sibling::weatherElement[11]/time[1]/elementValue/value)' ${wfile})
+	echo -e "${EMW}[${locations}${location}]${NON}  " && echo -ne ${weather}"\n" | sed 's/ //g'
 else # If not complete
 	echo -e "Weather Information Timeout\n"
 	> ${wfile} # eraser content
